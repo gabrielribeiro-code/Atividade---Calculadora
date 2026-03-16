95a4ee79ea8ea1501b2259f8bcb17be8f8bf5955
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-03-16 17:39:05 -0300

    add - Adicionei todas as funções no javascript para o funcionamento da calculadora e todas as operações.

---
+++ script.js
@@ -0,0 +1,53 @@
+/*Var */
+let numero1 = 0;
+let numero2 = 0;
+let operacao = 0;
+/*  */ 
+
+
+function clique (numero){
+    
+   let tela = document.getElementById('resultado').innerHTML;
+
+   if(numero == "+" || numero == "-" || numero == "*" || numero == "/"){
+    numero1 = tela;
+    operacao = numero;
+   }
+   document.getElementById('resultado').innerHTML = tela + numero;
+}
+
+function limpar(){
+    document.getElementById('resultado').innerHTML = " ";
+}
+
+function apagar(){
+    let resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+
+function calcular(){
+  
+    numero2 = document.getElementById('resultado').innerHTML;
+
+    numero1 = parseFloat(numero1);
+    numero2 = parseFloat(numero2);
+
+let resultado;
+
+if(operacao == "+"){
+        resultado = Number(numero1) + Number(numero2)
+    }
+else if(operacao == "-"){
+    resultado = Number(numero1) - Number(numero2)
+}
+
+else if(operacao == "*"){
+    resultado = Number(numero1) * Number(numero2)
+}
+
+else if(operacao == "/"){
+    resultado = Number(numero1) / Number(numero2)
+}
+    document.getElementById('resultado').innerHTML = resultado;
+}
