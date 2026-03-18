e82e5e58c1177d5061447a1e2cabf8abbd6c345b
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-03-18 08:18:10 -0300

    remove: removi erros que estava dando fazer as operações if e else

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 /*Var */
-let numero1 = 0;
-let numero2 = 0;
+let numero1 = (parseFloat);
+let numero2 = (parseFloat);
 let operacao = "";
+let numero = 0;
 /*  */ 
 
 
@@ -9,11 +10,11 @@ function clique (numero){
     
    let tela = document.getElementById('resultado').innerHTML;
 
-   if(numero == "+" || numero == "-" || numero == "*" || numero == "/"){
+   if(numero === "+" || numero === "-" || numero === "*" || numero === "/"){
     numero1 = tela;
     operacao = numero;
    }
-   document.getElementById('resultado').innerHTML = tela + numero;
+    document.getElementById('resultado').innerHTML = tela + numero;
 }
 
 function limpar(){
@@ -28,26 +29,4 @@ function apagar(){
 
 function calcular(){
   
-    numero2 = document.getElementById('resultado').innerHTML;
-
-    numero1 = parseFloat(numero1);
-    numero2 = parseFloat(numero2);
-
-let resultado;
-
-if(operacao == "+"){
-        resultado = Number(numero1) + Number(numero2)
-    }
-else if(operacao == "-"){
-    resultado = Number(numero1) - Number(numero2)
-}
-
-else if(operacao == "*"){
-    resultado = Number(numero1) * Number(numero2)
-}
-
-else if(operacao == "/"){
-    resultado = Number(numero1) / Number(numero2)
-}
-    document.getElementById('resultado').innerHTML = resultado;
-}
+   
